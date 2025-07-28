38bbd4fdffa2c9f24898c21dd37ddc8b57575ec0
Allen <allengeorge2400@gmail.com>
2025-07-28 20:58:18 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import React from 'react';
+import './index.css';
+
+
+const App = () =>{
+return (
+
+<main>
+<div className='pattern'>
+    <div className="wrapper">
+      <header>
+   
+        <img src="src/assets/hero-img.png" alt="Hero Banner" />
+        <h1>Find <span className='text-gradient'>Movies</span> You'll enjoy Without the Hassle</h1>
+      </header>
+
+
+      <p>Search</p>
+    </div>
+  </div>
+</main>
+
+)
+}
+
+
+
+export default App
+
+
+
+
